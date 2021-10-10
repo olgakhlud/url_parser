755843adecc1d0bdaeb818b06c04dac7eadd1a2e
Olga Khlud <khlud.olga@gmail.com>
2021-10-10 23:45:14 +0300

    'fix'

--- src/Parser.php
+++ src/Parser.php
@@ -5,7 +5,7 @@ namespace Parser;
 class Parser
 {
     public function get_host($url) {
-        $host = parse_url($url, 'host');
+        $host = parse_url($url,  PHP_URL_HOST);
         return $host;
     }
 }
